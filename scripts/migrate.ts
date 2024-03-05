2cffda6960645626af539ceaa6e7888e89e8d7ef
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-03-05 13:27:13 -0500

    Fix migrate script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,13 +1,13 @@
 // This should soon be a part of drizzle-kit
 // i.e.: it will be deprecated when `drizzle-kit apply` becomes a thing.
 
+import { scriptDb } from '@lib/database/db';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { exit } from 'process';
 
 try {
 	console.info('🚧 Applying database migration(s)...');
-	// Migrate
-	await migrate(db, { migrationsFolder: '' });
+	await migrate(scriptDb, { migrationsFolder: './migrations' });
 	console.info('🚀 Database migration(s) applied successfully!');
 } catch (error) {
 	console.error('❌ Database migration(s) failed (see error below).');
