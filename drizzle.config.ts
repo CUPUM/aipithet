80d7921c2fe03a8a6661f5586435e03e5501e3a0
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-03-05 13:28:31 -0500

    Fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
-import { register } from 'tsconfig-paths';
-import tsConfig from './.svelte-kit/tsconfig.json';
-import { DB_MIGRATIONS_FOLDER } from './scripts/common';
-
-/**
- * @see https://discord.com/channels/1043890932593987624/1125147482364584007/1154633287805976606
- */
-register({
-	baseUrl: './.svelte-kit',
-	paths: { ...tsConfig.compilerOptions.paths, '$env/*': ['../drizzle.config.ts'] },
-});
 
 export default defineConfig({
-	schema: ['./src/lib/db/schema/*'],
-	out: DB_MIGRATIONS_FOLDER,
+	schema: ['./src/lib/database/schema/*'],
+	out: './migrations',
 	driver: 'pg',
 	breakpoints: true,
 	dbCredentials: {
