89c706a94e255a8719a9fff94ab71425de372a5b
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-04-08 10:58:13 -0400

    Force bundling of argon2 in server components

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+	experimental: {
+		serverComponentsExternalPackages: ['@node-rs/argon2'],
+	},
 	webpack: (config) => {
 		// https://lucia-auth.com/getting-started/nextjs-app
 		config.externals.push('@node-rs/argon2', '@node-rs/bcrypt');
